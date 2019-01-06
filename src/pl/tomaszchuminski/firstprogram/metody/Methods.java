b210d820bf9fb39eb5fa7312cc713a3ea25de2bf
tomaszchuminski <tomasz.chuminski@gmail.com>
2019-01-06 17:27:51 +0100

    added Placek

--- src/pl/tomaszchuminski/firstprogram/metody/Methods.java
+++ src/pl/tomaszchuminski/firstprogram/metody/Methods.java
@@ -3,7 +3,7 @@ package pl.tomaszchuminski.firstprogram.metody;
 public class Methods {
     public static void main(String[] args) {
         hi();
-        hi("Karol", "Jacek", "Placek");
+        hi("Karol", "Jacek", "Pl");
     }
 
     private static void hi(String... names) {
