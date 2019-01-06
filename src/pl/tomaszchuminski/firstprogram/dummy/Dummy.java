0c7998dfbfde70b612b743ca29cd96c3fa0614fe
tomaszchuminski <tomasz.chuminski@gmail.com>
2019-01-06 22:27:58 +0100

    dummy commit2

--- src/pl/tomaszchuminski/firstprogram/dummy/Dummy.java
+++ src/pl/tomaszchuminski/firstprogram/dummy/Dummy.java
@@ -2,6 +2,6 @@ package pl.tomaszchuminski.firstprogram.dummy;
 
 public class Dummy {
     public static void main(String[] args) {
-        System.out.println("dummy");
+        System.out.println("dummy1");
     }
 }
