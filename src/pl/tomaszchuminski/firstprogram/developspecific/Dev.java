d4b4331097177600b82a70bb6932082dc885f33a
tomaszchuminski <tomasz.chuminski@gmail.com>
2019-01-16 22:30:38 +0100

    sout dev

--- src/pl/tomaszchuminski/firstprogram/developspecific/Dev.java
+++ src/pl/tomaszchuminski/firstprogram/developspecific/Dev.java
@@ -1,4 +1,7 @@
 package pl.tomaszchuminski.firstprogram.developspecific;
 
 public class Dev {
+    public static void main(String[] args) {
+        System.out.println("To jest klasa charakterystyczna dla bramcha develop");
+    }
 }
