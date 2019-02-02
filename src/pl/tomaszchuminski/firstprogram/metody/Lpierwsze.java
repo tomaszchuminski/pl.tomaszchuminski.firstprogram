fe0150fd22f4a033ef7db141cb8a91ae357658e8
tomaszchuminski <tomasz.chuminski@gmail.com>
2019-02-02 14:38:27 +0100

    working properly

--- src/pl/tomaszchuminski/firstprogram/metody/Lpierwsze.java
+++ src/pl/tomaszchuminski/firstprogram/metody/Lpierwsze.java
@@ -6,18 +6,31 @@ public class Lpierwsze {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        //int x = scan.nextInt();
-        float x = 8;
+        int x = scan.nextInt();
+        //int x = ;
 
-        for (float i = x; i > 0; i--) {
-           // i = x;
+        for (int i = x/2 + 1; i >= 1; i--) {
+
             //System.out.println("x= " + x);
             //System.out.println("i= " + i);
-            float w = x/i;
-            float s = x%i;
+            int w = x%i;
+
             //System.out.println("s= " + s);
-            if (w >= 2) {
+            if (w == 0 && i != 1 && x != 2) {                           //if w=0 -> l.pierwsza
                 System.out.println(x + " nie jest pierwsza");
+                System.out.println(i + " to pierwszy dzielnik");
+                i = 0;                                                  //i=0 to break loop
+            }
+            if (i == 2 && w != 0) {                 //if i=2 hasnt been breaken(see above) -> i=2 and w!=0 -> l.pierwsza
+                System.out.println(x + " jest pierwsza");
+            }
+            else if (x==1){
+                System.out.println(x + " nie jest pierwsza z DEFINICJI");
+                i = 0;
+            }
+            else if (x==2){
+                System.out.println(x + " jest pierwsza");
+                i = 0;
             }
 
         }
